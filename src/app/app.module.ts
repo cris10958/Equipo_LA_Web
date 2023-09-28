a98e6a1e3f591a06dc6e1070bd7e40592013fdac
alexander contreras trujillo <a.contrerast@uniandes.edu.co>
2023-09-28 02:05:49 -0500

    se crea la lista de rutinas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RoutineListComponent } from './routine-list/routine-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutineListComponent,
   ],
   imports: [
     BrowserModule,
